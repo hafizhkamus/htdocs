9901f27936a86684ff3efcf1ff6c0c325756c683
hafizhkamus <hafizhkamus13@gmail.com>
2021-01-23 14:43:43 +0700

    C

--- praktikum11/namafile.php
+++ praktikum11/namafile.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<form method="post" action="hasil.php">
+<form method="post">
     <div class="container">
         <div class="text-center">
             <h1 class="font-weight-bold">Daftar NIlai UN Siswa</h1>
@@ -65,6 +65,28 @@
         </div>
     </div>
 </form>
+
+<div class="container">
+        <div class="card text-center pt-5">
+        <p id="hasil">
+        <?php 
+            $name = $_POST['nama'];
+            $indo = $_POST['indo'];
+            $math = $_POST['math'];
+            $inggris = $_POST['inggris'];
+            $tk = $_POST['tk'];
+            $hasil = $indo+$math+$inggris+$tk;
+
+            echo "Nama : $name  <br />"; 
+            echo "Nilai Bahasa Indonesia : $indo <br />"; 
+            echo "Nilai Matematika :  $math <br />"; 
+            echo "Nilai Bahasa Inggris : $inggris <br />"; 
+            echo "Nilai Teknik Keahlian : $tk <br />"; 
+            echo "Nilai Keseluruhan : $hasil <br />"; 
+            ?>    
+        </p>
+        </div>
+    </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
